2d0e3b9cc8fd8a7e3859aec74fe4589530d292c1
andrewdark <samsonov.a@ukr.net>
2026-08-17 08:44:31 +0300

    Add dto

---
+++ accounts/src/main/java/ua/pp/darknsoft/accounts/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package ua.pp.darknsoft.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    private String statusCode;
+    private String statusMsg;
+}
