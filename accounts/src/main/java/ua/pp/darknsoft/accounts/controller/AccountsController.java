02853e17a5d6356df68273275049709a2eda049d
andrewdark <samsonov.a@ukr.net>
2026-08-21 14:02:33 +0300

    Add Update method for controller layer

--- accounts/src/main/java/ua/pp/darknsoft/accounts/controller/AccountsController.java
+++ accounts/src/main/java/ua/pp/darknsoft/accounts/controller/AccountsController.java
@@ -42,4 +42,18 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
 }
