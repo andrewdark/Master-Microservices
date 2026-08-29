2dafc9eac9ae57bcb19638c717580efc6fdbb1ee
andrewdark <samsonov.a@ukr.net>
2026-08-29 08:52:59 +0300

    Create find method in repo-interface

--- cards/src/main/java/ua/pp/darknsoft/cards/repository/CardRepository.java
+++ cards/src/main/java/ua/pp/darknsoft/cards/repository/CardRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.pp.darknsoft.cards.entity.Card;
 
+import java.util.Optional;
+
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    Optional<Card> findByMobileNumber(String mobileNumber);
+
+    Optional<Card> findByCardNumber(String cardNumber);
 }
