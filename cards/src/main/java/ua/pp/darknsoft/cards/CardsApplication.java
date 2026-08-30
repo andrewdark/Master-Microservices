06d9ddbfa56f75a08e109a992ffcb8e8b71214c4
andrewdark <samsonov.a@ukr.net>
2026-08-30 13:55:56 +0300

    Add swagger - Open API

--- cards/src/main/java/ua/pp/darknsoft/cards/CardsApplication.java
+++ cards/src/main/java/ua/pp/darknsoft/cards/CardsApplication.java
@@ -1,11 +1,35 @@
 package ua.pp.darknsoft.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "DarkyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Andrew Dark",
+						email = "info@darknsoft.pp.ua",
+						url = "https://www.darknsoft.pp.ua"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.darknsoft.pp.ua"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "DarkyBank Cards microservice REST API Documentation",
+				url = "https://www.darknsoft.pp.ua/swagger-ui.html"
+		))
 public class CardsApplication {
 
 	public static void main(String[] args) {
