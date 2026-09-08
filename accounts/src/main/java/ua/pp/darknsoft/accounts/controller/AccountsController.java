819cd3bc4f84ec726a6c9ed73b03f2744242d8e2
andrewdark <samsonov.a@ukr.net>
2026-09-08 10:23:17 +0300

    Add java-info GET-method through the environment

--- accounts/src/main/java/ua/pp/darknsoft/accounts/controller/AccountsController.java
+++ accounts/src/main/java/ua/pp/darknsoft/accounts/controller/AccountsController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -36,8 +37,14 @@ import ua.pp.darknsoft.accounts.service.IAccountsService;
 @Validated
 public class AccountsController {
 
+    /**
+     * Not recommended using @Value
+     * Just if 1 or 2 props
+     */
     @Value("${build.version}")
     private String buildVersion;
+
+    private final Environment environment;
 
     private final IAccountsService iAccountsService;
 
@@ -185,5 +192,25 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(summary = "Get JAVA version Info", description = "Get JAVA version Info")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
+    }
+    )
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 }
