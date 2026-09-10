453061ccaa0e578e1debfa8ff97e564e580c8b59
andrewdark <samsonov.a@ukr.net>
2026-09-10 10:12:20 +0300

    connect cards ms to configserver

--- cards/src/main/java/ua/pp/darknsoft/cards/CardsApplication.java
+++ cards/src/main/java/ua/pp/darknsoft/cards/CardsApplication.java
@@ -9,10 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import ua.pp.darknsoft.cards.dto.CardsContactInfoRDto;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@EnableConfigurationProperties(value = {CardsApplication.class})
+@EnableConfigurationProperties(value = {CardsContactInfoRDto.class})
 @OpenAPIDefinition(
         info = @Info(
                 title = "Cards microservice REST API Documentation",
