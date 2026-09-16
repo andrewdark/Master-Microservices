6f76fc2cba273bc7d4303c364b8c0a18167b75df
andrewdark <samsonov.a@ukr.net>
2026-09-16 17:51:32 +0300

    SECTION-10 : circuitbreaker-reactor-resilience4j dependence

--- gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/GatewayserverApplication.java
@@ -21,7 +21,9 @@ public class GatewayserverApplication {
                 .route(p -> p
                         .path("/darkybank/accounts/**")
                         .filters(f -> f.rewritePath("/darkybank/accounts/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCB"))
+                        )
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/darkybank/loans/**")
