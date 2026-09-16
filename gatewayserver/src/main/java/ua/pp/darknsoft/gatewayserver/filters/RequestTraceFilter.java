b3b3894a107bc109a9771512cb1e186c2db8f679
andrewdark <samsonov.a@ukr.net>
2026-09-16 07:23:23 +0300

    add request filter

---
+++ gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/filters/RequestTraceFilter.java
@@ -0,0 +1,48 @@
+package ua.pp.darknsoft.gatewayserver.filters;
+
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(1)
+@Component
+public class RequestTraceFilter implements GlobalFilter {
+    private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
+
+    private final FilterUtility filterUtility;
+
+    public RequestTraceFilter(FilterUtility filterUtility) {
+        this.filterUtility = filterUtility;
+    }
+
+    @Override
+    @NonNull
+    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(requestHeaders)) {
+            logger.debug("darkyBank-correlation-id found in RequestTraceFilter : {}",
+                    filterUtility.getCorrelationId(requestHeaders));
+        } else {
+            String correlationID = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationID);
+            logger.debug("darkyBank-correlation-id generated in RequestTraceFilter : {}", correlationID);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+        return filterUtility.getCorrelationId(requestHeaders) != null;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+
+}
---
+++ gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/filters/ResponseTraceFilter.java
@@ -0,0 +1,4 @@
+package ua.pp.darknsoft.gatewayserver.filters;
+
+public class ResponseTraceFilter {
+}
