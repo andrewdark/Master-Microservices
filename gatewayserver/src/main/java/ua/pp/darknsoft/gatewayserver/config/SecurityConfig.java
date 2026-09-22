61c5a074e42350923d2ab5ced4c867a28531d3f3
andrewdark <samsonov.a@ukr.net>
2026-09-22 12:40:49 +0300

    Add simple security config for testing

--- gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/ua/pp/darknsoft/gatewayserver/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package ua.pp.darknsoft.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,6 +15,14 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
 
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+                        .pathMatchers("/darkybank/accounts/**").authenticated()
+                        .pathMatchers("/darkybank/cards/**").authenticated()
+                        .pathMatchers("/darkybank/loans/**").authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(Customizer.withDefaults()));
+
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
         return serverHttpSecurity.build();
     }
 }
