e929d1017779ac1af62513a925badb977809218c
andrewdark <samsonov.a@ukr.net>
2026-09-24 14:23:40 +0300

    SECTION 12 preparation. Rabbit mq

---
+++ message/src/main/java/ua/pp/darknsoft/message/MessageApplication.java
@@ -0,0 +1,13 @@
+package ua.pp.darknsoft.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
